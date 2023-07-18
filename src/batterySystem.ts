7fa40fd9751af3f11b01bc4b659f5898cad4c559
fijiro <jiro.protonmail@protonmail.com>
2023-07-18 15:12:36 +0300

    Working battery models removed redundant AAA Battery Added missing sights

--- src/batterySystem.ts
+++ src/batterySystem.ts
@@ -3,12 +3,11 @@ import { DependencyContainer } from "tsyringe";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger"
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { CustomItemService } from "@spt-aki/services/mod/CustomItemService";
-import { LocaleDetails, NewItemFromCloneDetails } from "@spt-aki/models/spt/mod/NewItemDetails";
+import { NewItemFromCloneDetails } from "@spt-aki/models/spt/mod/NewItemDetails";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { BaseClasses } from "@spt-aki/models/enums/BaseClasses"
 import { ItemHelper } from "@spt-aki/helpers/ItemHelper"
 import * as config from "../config/config.json";
-import { LocaleService } from "../types/services/LocaleService";
 
 class Mod implements IPostDBLoadMod {
     private batteryType = "2";
@@ -27,29 +26,31 @@ class Mod implements IPostDBLoadMod {
         const rchblBatteryID = "590a358486f77429692b2790";
         items[aaBatteryID]._props.MaxResource = 100;
         items[aaBatteryID]._props.Resource = 100;
-        items[dBatteryID]._props.MaxResource = 100;
-        items[dBatteryID]._props.Resource = 100;
         items[rchblBatteryID]._props.MaxResource = 100;
         items[rchblBatteryID]._props.Resource = 100;
-        items[rchblBatteryID]._props.Prefab.path = "assets/content/items/barter/item_barter_medical_defibrillator/item_barter_medical_defibrillator.bundle";
+        items[rchblBatteryID]._props.Prefab.path = "batteries/cr123.bundle"
+        items[dBatteryID]._props.MaxResource = 100;
+        items[dBatteryID]._props.Resource = 100;
+        items[dBatteryID]._props.Prefab.path = "batteries/cr2032.bundle";
 
         //Credit to Jehree! // 16 locales, wtf?
         for (const locale of locales) {
-            locale[`${dBatteryID} Name`] = "CR123 Battery";
-            locale[`${dBatteryID} ShortName`] = "CR123";
-            locale[`${dBatteryID} Description`] = "A singular CR123A Battery. These are commonly used in military and hunting sights.";
-            locale[`${rchblBatteryID} Name`] = "CR2032 Battery";
-            locale[`${rchblBatteryID} ShortName`] = "CR2032";
-            locale[`${rchblBatteryID} Description`] = "A singular CR2032 Battery. These are commonly used in military and hunting sights.";
+            locale[`${rchblBatteryID} Name`] = "CR123 Rechargeable Battery";
+            locale[`${rchblBatteryID} ShortName`] = "CR123";
+            locale[`${rchblBatteryID} Description`] = "A singular CR123A Battery. These are commonly used in military and hunting sights.";
+            locale[`${dBatteryID} Name`] = "CR2032 Battery";
+            locale[`${dBatteryID} ShortName`] = "CR2032";
+            locale[`${dBatteryID} Description`] = "A singular CR2032 Battery. These are commonly used in military and hunting sights.";
         };
-        const AAABattery: NewItemFromCloneDetails = {
+
+        /*const AAABattery: NewItemFromCloneDetails = {
             itemTplToClone: "5672cb124bdc2d1a0f8b4568", //aa battery
             overrideProperties: {
                 MaxResource: 100,
                 Resource: 100,
                 InsuranceDisabled: true,
                 Prefab: {
-                    path: "assets/content/items/barter/battery_aa/item_battery_aa.bundle",
+                    path: "batteries/cr2032.bundle",
                     rcid: ""
                 },
                 Unlootable: true,
@@ -74,7 +75,7 @@ class Mod implements IPostDBLoadMod {
             }
         }
         CustomItem.createItemFromClone(AAABattery); //Basically calls the function and tell the server to add our Cloned new item into the server
-        
+        */
         // huge thanks and credit to jbs4mx! https://github.com/jbs4bmx/SpecialSlots/
         const pockets = items["627a4e6b255f7527fb05a0f6"];
         pockets._props.Slots[0]._props.filters[0].Filter.push(dBatteryID, rchblBatteryID, aaBatteryID);
@@ -83,28 +84,28 @@ class Mod implements IPostDBLoadMod {
         //S I C C case now fits batteries in it
         items["5d235bb686f77443f4331278"]._props.Grids[0]._props.filters[0].Filter.push(dBatteryID, rchblBatteryID, aaBatteryID);
         //add battery slots to wanted items
-        for (let i in items) {  //check that item isn't NightVision or ThermalVision, and is either NVG/Thermal sight, NVG/Thermal goggles or T-7.
-            if ((items[i]._id != BaseClasses.NIGHTVISION && items[i]._id != "5d21f59b6dbe99052b54ef83" && (items[i]._parent == BaseClasses.SPECIAL_SCOPE
-                || items[i]._parent == BaseClasses.NIGHTVISION || items[i]._parent == "5d21f59b6dbe99052b54ef83")) // headwear
+        for (let i in items) {  //check that item isn't NightVision or ThermalVision and requires a battery, and is either NVG/Thermal sight, NVG/Thermal goggles or T-7.
+            if ((items[i]._id != BaseClasses.NIGHTVISION && items[i]._id != "5d21f59b6dbe99052b54ef83"
+                && !config.NoBattery.includes(items[i]._id)
+                && (items[i]._parent == BaseClasses.SPECIAL_SCOPE || items[i]._parent == BaseClasses.NIGHTVISION || items[i]._parent == "5d21f59b6dbe99052b54ef83")) // headwear
                 || (items[i]._parent == BaseClasses.COLLIMATOR || items[i]._parent == BaseClasses.COMPACT_COLLIMATOR)) { // sight
 
                 if (config.AA.includes(items[i]._id))
                     this.batteryType = aaBatteryID; //AA Battery ID                
                 else if (config.CR123.includes(items[i]._id))
-                    this.batteryType = dBatteryID;
+                    this.batteryType = rchblBatteryID; //CR123, for now rchbl battery
                 else if (config.CR2032.includes(items[i]._id))
-                    this.batteryType = rchblBatteryID;
+                    this.batteryType = dBatteryID; //CR2032, for now d battery
                 else if (config.CR1225.includes(items[i]._id))
-                    this.batteryType = rchblBatteryID; //CR1225 Battery ID, for now rchbl battery
+                    this.batteryType = dBatteryID;
                 else if (config.CR1632.includes(items[i]._id))
-                    this.batteryType = rchblBatteryID;
+                    this.batteryType = dBatteryID;
                 else {
-                    logger.warning("Item " + items[i]._name + " has no defined battery!");
+                    logger.warning("BatterySystem: Item " + items[i]._name + " has no defined battery, defaulting to CR2032!");
                     this.batteryType = dBatteryID;
                 }
                 for (const locale of locales) { // Item description now includes the battery type
                     const newDescription = "Uses " + locale[`${this.batteryType} Name`] + "\n\n" + locale[`${items[i]._id} Description`];
-                    logger.info(newDescription.toString() + "\n\n\n");
                     locale[`${items[i]._id} Description`] = newDescription;
                 }
                 //create new slot for battery
@@ -135,7 +136,7 @@ class Mod implements IPostDBLoadMod {
         hideoutProduction.push(
             {
                 // craft triple a battery
-                "_id": "aaa-battery0",
+                "_id": "cr2032Craft0",
                 "areaType": 10,
                 "requirements": [
                     {
@@ -144,8 +145,15 @@ class Mod implements IPostDBLoadMod {
                         "type": "Area"
                     },
                     {
-                        "templateId": "5672cb124bdc2d1a0f8b4568", //aa battery
-                        "count": 3,
+                        "templateId": "544fb5454bdc2df8738b456a", //multiTool
+                        "count": 1,
+                        "isFunctional": false,
+                        "isEncoded": false,
+                        "type": "Tool"
+                    },
+                    {
+                        "templateId": aaBatteryID, //aa battery
+                        "count": 1,
                         "isFunctional": false,
                         "isEncoded": false,
                         "type": "Item"
@@ -154,15 +162,15 @@ class Mod implements IPostDBLoadMod {
                 "productionTime": 3600, // seconds
                 "needFuelForAllProductionTime": false,
                 "locked": false,
-                "endProduct": "aaa-battery",
+                "endProduct": dBatteryID,
                 "continuous": false,
-                "count": 1,
+                "count": 2,
                 "productionLimitCount": 0,
                 "isEncoded": false
             },
             {
                 // Induction!
-                "_id": "aaa-battery1",
+                "_id": "cr123Recharge0",
                 "areaType": 2,
                 "requirements": [
                     {
@@ -185,24 +193,24 @@ class Mod implements IPostDBLoadMod {
                         "type": "Item"
                     },
                     {
-                        "templateId": dBatteryID,
+                        "templateId": rchblBatteryID,
                         "count": 1,
                         "isFunctional": false,
                         "isEncoded": false,
                         "type": "Item"
                     }
                 ],
-                "productionTime": 1800, // seconds
+                "productionTime": 600, // seconds
                 "needFuelForAllProductionTime": false,
                 "locked": false,
-                "endProduct": "aaa-battery",
+                "endProduct": rchblBatteryID,
                 "continuous": false,
                 "count": 1,
                 "productionLimitCount": 0,
                 "isEncoded": false
             },
             { // Car Battery!
-                "_id": "aaa-battery2",
+                "_id": "cr123Recharge1",
                 "areaType": 2,
                 "requirements": [
                     {
@@ -225,24 +233,24 @@ class Mod implements IPostDBLoadMod {
                         "type": "Item"
                     },
                     {
-                        "templateId": "aaa-battery",
+                        "templateId": rchblBatteryID,
                         "count": 2,
                         "isFunctional": false,
                         "isEncoded": false,
                         "type": "Item"
                     }
                 ],
-                "productionTime": 1800, // seconds
+                "productionTime": 600, // seconds
                 "needFuelForAllProductionTime": false,
                 "locked": false,
-                "endProduct": "aaa-battery",
+                "endProduct": rchblBatteryID,
                 "continuous": false,
                 "count": 2,
                 "productionLimitCount": 0,
                 "isEncoded": false
             },
             { // Normal charging :(
-                "_id": "aaa-battery3",
+                "_id": "cr123Recharge2",
                 "areaType": 10,
                 "requirements": [
                     {
@@ -265,7 +273,7 @@ class Mod implements IPostDBLoadMod {
                         "type": "Item"
                     },
                     {
-                        "templateId": "aaa-battery",
+                        "templateId": rchblBatteryID,
                         "count": 3,
                         "isFunctional": false,
                         "isEncoded": false,
@@ -275,14 +283,14 @@ class Mod implements IPostDBLoadMod {
                 "productionTime": 600, // seconds
                 "needFuelForAllProductionTime": false,
                 "locked": false,
-                "endProduct": "aaa-battery",
+                "endProduct": rchblBatteryID,
                 "continuous": false,
                 "count": 3,
                 "productionLimitCount": 0,
                 "isEncoded": false
             },
         );
-        logger.info("BatterySystem has been applied!");
+        logger.success("BatterySystem has been applied!");
     }
 }
 
